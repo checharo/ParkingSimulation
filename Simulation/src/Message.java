b894e56ae81053899c3e1c1e88808bcaf9b3c188
Cesar Martinez D Granda <checharo@gmail.com>
2013-10-16 16:32:39 +1100

    Added everything again

---
+++ Simulation/src/Message.java
@@ -0,0 +1,79 @@
+
+import java.util.Vector;
+
+
+/**
+ *
+ * @author cesar
+ */
+public class Message {
+    
+    private String header;
+    private String content;
+    /* Stack for routing back a reply to a message or stop flooding */
+    private Vector stack;
+
+    public Message() {
+        this.stack = new Vector();
+    }
+    
+    /* Sends a message without sender, (intended for central) */
+    public Message(String header, String content) {
+        this.header = header;
+        this.content = content;
+        this.stack = new Vector();
+    }
+    
+    public Message(String header, String content, Sensor sender) {
+        this.header = header;
+        this.content = content;
+        this.stack = new Vector();
+        stack.add(sender);
+    }
+    
+    /**
+     * @return the header
+     */
+    public String getHeader() {
+        return header;
+    }
+
+    /**
+     * @param header the header to set
+     */
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+    public void push(Sensor m) {
+        stack.add(m);
+    }
+    
+    /**
+     * Pops the top sensor in the stack. If the stack is already empty then
+     * it returns null;
+     * @return 
+     */
+    public Sensor pop() {
+        if (stack.isEmpty())
+            return null;
+        else
+            return (Sensor) stack.remove(stack.size() - 1);
+    }
+    
+    
+}
