98e4952fbcbc145d774513e6de617beb12d5d6bc
Cesar Martinez D Granda <checharo@gmail.com>
2013-10-16 16:01:59 +1100

    Added the first files to the project

---
+++ src/Central.java
@@ -0,0 +1,40 @@
+
+/**
+ * Represents the central control of the network. 
+ * @author César Martínez D'Granda (cesarm@student.unimelb.edu.au)
+ */
+public class Central {
+    
+    /* Represents the sensor matrix. */
+    private Sensor[][] matrix;
+    private MainFrame mainFrame;
+    
+    public Central(Sensor[][] matrix, MainFrame mainFrame) {
+        this.matrix = matrix;
+        this.mainFrame = mainFrame;
+    }
+    
+    public Sensor[][] getMatrix() {
+        return matrix;
+    }
+    
+    /**
+     * Receives a message from a Sensor. Virtual Sensors will call this method
+     * directly, while the Real Sensors will send the message through the 
+     * data input connection and then this method will be called.
+     * @param m The message received.
+     */
+    public synchronized void recieveMessage(Message m) {
+        /* DO NOTHING XD!!! ... for the moment */
+        
+        /* Reply back acknowledge */
+        Sensor top = m.pop();
+        m.setHeader("reply-ok");
+        m.setContent("");
+        top.receiveMessage(m);
+    }
+    
+    public synchronized void refreshCanvas() {
+        mainFrame.repaintCanvas();
+    }
+}
